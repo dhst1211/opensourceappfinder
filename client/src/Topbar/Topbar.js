54530828d41017cf953fb5283262b2f2fcad089c
dhst1211 <dhst1211@example.com>
2020-04-10 20:36:32 +0800

    add link to github

--- client/src/Topbar/Topbar.js
+++ client/src/Topbar/Topbar.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import './Topbar.scss';
 
@@ -14,6 +15,7 @@ export default class Topbar extends Component {
           <Typography variant="h6" className="appbar-title">
             Opensource App Finder
           </Typography>
+          <Button color="inherit" href="https://github.com/dhst1211/opensourceappfinder">Code</Button>
         </Toolbar>
       </AppBar>
     )
